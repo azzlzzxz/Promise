84126fe61b2f51d1ff9782e91697675dc0bde4fe
xinxu <azzlzzxz@gmail.com>
2022-12-22 09:39:34 +0800

    feat: promise

--- 5.promise/promise.js
+++ 5.promise/promise.js
@@ -2,7 +2,7 @@
  * @Author: xinxu
  * @Date: 2022-12-21 14:13:30
  * @LastEditors: xinxu
- * @LastEditTime: 2022-12-21 15:06:51
+ * @LastEditTime: 2022-12-21 22:36:43
  * @FilePath: /Promise/5.promise/promise.js
  */
 const STATUS = {
@@ -103,11 +103,27 @@ class Promise {
     // catch就是then的简写，只有失败没有成功
     return this.then(null, err);
   }
+
+  finally(callback) {
+    return this.then(
+      (data) => {
+        // 返回promise等待其执行完毕
+        return Promise.resolve(callback()).then(() => data);
+      },
+      (err) => {
+        return Promise.resolve(callback()).then(() => {
+          throw err;
+        });
+      }
+    );
+  }
+
   static resolve(value) {
     return new Promise((resolve, reject) => {
       resolve(value);
     });
   }
+
   static reject(reason) {
     return new Promise((resolve, reject) => {
       reject(reason);
@@ -116,23 +132,45 @@ class Promise {
 
   static all(promises) {
     return new Promise((resolve, reject) => {
-      let result = [];
-      let times = 0;
-      //做定时器的原因是：[1,getName,getAge,2]循环走到2，那么getName,getAge是empty，因为是异步的，所以不能用result.length === promises.length作为判断条件
-      function processData(i, val) {
-        result[i] = val;
-        if (times++ === promises.length) {
-          resolve(result);
+      if (Array.isArray(promises)) {
+        let result = [];
+        let times = 0;
+        const processData = (i, val) => {
+          result[i] = val;
+          if (result.length === promises.length) {
+            resolve(result);
+          }
+        };
+        for (let i = 0; i < promises.length; i++) {
+          if (isPromise(promises[i])) {
+            promises[i].then((data) => {
+              processData(i, data);
+            }, reject);
+          } else {
+            processData(i, promises[i]);
+          }
         }
+      } else {
+        return reject(new TypeError("参数错误"));
       }
-      for (let i = 0; i < promises.length; i++) {
-        if (isPromise(promises[i])) {
-          promises[i].then((data) => {
-            processData(i, data);
-          }, reject);
-        } else {
-          processData(i, promises[i]);
+    });
+  }
+
+  static race(promises) {
+    return new Promise((resolve, reject) => {
+      if (Array.isArray(promises)) {
+        if (promises.length > 0) {
+          for (let i = 0; i < promises.length; i++) {
+            let result = promises[i];
+            if (isPromise(result)) {
+              return result.then(resolve, reject);
+            } else {
+              return resolve(result);
+            }
+          }
         }
+      } else {
+        return reject(new TypeError("参数错误"));
       }
     });
   }
--- 5.promise/test.js
+++ 5.promise/test.js
@@ -2,14 +2,46 @@
  * @Author: xinxu
  * @Date: 2022-12-21 11:08:49
  * @LastEditors: xinxu
- * @LastEditTime: 2022-12-21 15:13:55
+ * @LastEditTime: 2022-12-21 22:26:56
  * @FilePath: /Promise/5.promise/test.js
  */
-const Promise = require("./promise");
-const fs = require("fs");
+// const Promise = require("./promise");
 
-const getName = fs.readFile("name.txt", "utf8", (err, data) => {});
-const getAge = fs.readFile("age.txt", "utf8", (err, data) => {});
-Promise.all([1, getName, getAge, 2]).then((data) => {
-  console.log("data", data);
+const promise1 = Promise.resolve(3);
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("success");
+  }, 1000);
 });
+const promise3 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("yes");
+  }, 2000);
+});
+
+Promise.all([1, promise1, promise2]).then((values) => {
+  console.log(values);
+});
+
+Promise.race([1, promise1, promise2]).then((data) => {
+  console.log(data);
+});
+
+Promise.resolve(123)
+  .finally(() => {
+    // 这里传入的函数 无论如何都会执行
+    console.log("finally");
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        reject("ok");
+      }, 5000);
+    });
+  })
+  .then(
+    (data) => {
+      console.log("s:" + data);
+    },
+    (err) => {
+      console.log("e:" + err);
+    }
+  );
